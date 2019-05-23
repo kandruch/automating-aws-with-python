aab4160813f3d934607788443632fc283f806f61
EC2 Default User <ec2-user@ip-172-31-79-124.ec2.internal>
2019-05-23 01:45:53 +0000

    Added ETag function

--- 01-webotron/webotron/bucket.py
+++ 01-webotron/webotron/bucket.py
@@ -4,35 +4,35 @@
 
 from pathlib import Path
 import mimetypes
-# from functools import reduce
+from functools import reduce
 
-# import boto3
+import boto3
 from botocore.exceptions import ClientError
 
 import util
 
-# from hashlib import md5
-# from webotron import util
+from hashlib import md5
+from webotron import util
 
 
 class BucketManager:
     """Manage an S3 Bucket."""
 
-    # CHUNK_SIZE = 8388608
+    CHUNK_SIZE = 8388608
 
     def __init__(self, session):
         """Create a BucketManager object."""
         self.session = session
         self.s3 = self.session.resource('s3')
-        # self.transfer_config = boto3.s3.transfer.TransferConfig(
-        #     multipart_chunksize=self.CHUNK_SIZE,
-        #     multipart_threshold=self.CHUNK_SIZE
-        # )
-        # self.manifest = {}
+        self.transfer_config = boto3.s3.transfer.TransferConfig(
+            multipart_chunksize=self.CHUNK_SIZE,
+            multipart_threshold=self.CHUNK_SIZE
+        )
+        self.manifest = {}
 
-    # def get_bucket(self, bucket_name):
-    #     """Get a bucket by name."""
-    #     return self.s3.Bucket(bucket_name)
+    def get_bucket(self, bucket_name):
+        """Get a bucket by name."""
+        return self.s3.Bucket(bucket_name)
 
     def get_region_name(self, bucket):
         """Get the bucket's region name."""
@@ -106,50 +106,50 @@ class BucketManager:
             }
         })
 
-    # def load_manifest(self, bucket):
-    #     """Load manifest for caching purposes."""
-    #     paginator = self.s3.meta.client.get_paginator('list_objects_v2')
-    #     for page in paginator.paginate(Bucket=bucket.name):
-    #         for obj in page.get('Contents', []):
-    #             self.manifest[obj['Key']] = obj['ETag']
+    def load_manifest(self, bucket):
+        """Load manifest for caching purposes."""
+        paginator = self.s3.meta.client.get_paginator('list_objects_v2')
+        for page in paginator.paginate(Bucket=bucket.name):
+            for obj in page.get('Contents', []):
+                self.manifest[obj['Key']] = obj['ETag']
 
-    # @staticmethod
-    # def hash_data(data):
-    #     """Generate md5 hash for data."""
-    #     hash = md5()
-    #     hash.update(data)
+    @staticmethod
+    def hash_data(data):
+        """Generate md5 hash for data."""
+        hash = md5()
+        hash.update(data)
 
-    #     return hash
+        return hash
 
-    # def gen_etag(self, path):
-    #     """Generate etag for file."""
-    #     hashes = []
+    def gen_etag(self, path):
+        """Generate etag for file."""
+        hashes = []
 
-    #     with open(path, 'rb') as f:
-    #         while True:
-    #             data = f.read(self.CHUNK_SIZE)
+        with open(path, 'rb') as f:
+            while True:
+                data = f.read(self.CHUNK_SIZE)
 
-    #             if not data:
-    #                 break
+                if not data:
+                    break
 
-    #             hashes.append(self.hash_data(data))
+                hashes.append(self.hash_data(data))
 
-    #     if not hashes:
-    #         return
-    #     elif len(hashes) == 1:
-    #         return '"{}"'.format(hashes[0].hexdigest())
-    #     else:
-    #         digests = (h.digest() for h in hashes)
-    #         hash = self.hash_data(reduce(lambda x, y: x + y, digests))
-    #         return '"{}-{}"'.format(hash.hexdigest(), len(hashes))
+        if not hashes:
+            return
+        elif len(hashes) == 1:
+            return '"{}"'.format(hashes[0].hexdigest())
+        else:
+            digests = (h.digest() for h in hashes)
+            hash = self.hash_data(reduce(lambda x, y: x + y, digests))
+            return '"{}-{}"'.format(hash.hexdigest(), len(hashes))
 
     def upload_file(self, bucket, path, key):
         """Upload path to s3_bucket at key."""
         content_type = mimetypes.guess_type(key)[0] or 'text/plain'
 
-        # etag = self.gen_etag(path)
-        # if self.manifest.get(key, '') == etag:
-        #     return
+        etag = self.gen_etag(path)
+        if self.manifest.get(key, '') == etag:
+            return
 
         return bucket.upload_file(
             path,
@@ -157,13 +157,13 @@ class BucketManager:
             ExtraArgs={
                 'ContentType': content_type
             },
-            # Config=self.transfer_config
+            Config=self.transfer_config
         )
 
     def sync(self, pathname, bucket_name):
         """Sync contents of path to bucket."""
         bucket = self.s3.Bucket(bucket_name)
-        # self.load_manifest(bucket)
+        self.load_manifest(bucket)
 
         root = Path(pathname).expanduser().resolve()
 

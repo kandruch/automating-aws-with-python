3e489f7398bbdfd5672bb383048117862a03d078
EC2 Default User <ec2-user@ip-172-31-79-124.ec2.internal>
2019-09-21 13:03:02 +0000

    Add example autoscaling lambda

---
+++ 01-webotron/autoscale_example.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+import boto3
+session = boto3.Session(profile_name='default')
+as_client.describe_auto_scaling_groups()
+as_client = session.client('autoscaling')
+as_client.describe_auto_scaling_groups()
+as_client.describe_policy
+as_client.describe_policy()
+as_client.describe_policies()
+get_ipython().run_line_magic('save', 'autoscale_example.py 1-9')
